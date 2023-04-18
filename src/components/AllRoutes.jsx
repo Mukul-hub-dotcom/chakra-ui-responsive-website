34de2e856662c032b31d18293b93f903506ae860
Mukul <65118538+Mukul-hub-dotcom@users.noreply.github.com>
2023-04-18 15:19:17 +0530

    added browser routes in app

---
+++ src/components/AllRoutes.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Route,Routes } from 'react-router-dom'
+import { Navbar } from './Navbar'
+import Home from '../pages/Home'
+import ProductCard from './ProductCard'
+import About from '../pages/About'
+import Footer from './Footer'
+import Contact from '../pages/Contact'
+const AllRoutes = () => {
+  return (
+    <div>
+        <Navbar/>
+        <div style={{margin:'2rem'}}>
+        <Routes >
+            <Route path='/'element={<Home/>}></Route>
+            <Route path='/about'element={<About/>}></Route>
+            <Route path='/contact'element={<Contact/>}></Route>
+        </Routes>
+        </div>
+        
+        {/* <Footer/> */}
+    </div>
+  )
+}
+
+export default AllRoutes
