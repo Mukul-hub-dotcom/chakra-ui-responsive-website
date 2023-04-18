455d03f2bcf66ba896f49547ede9640b08628e0a
Mukul <65118538+Mukul-hub-dotcom@users.noreply.github.com>
2023-04-18 15:08:07 +0530

    made navbar and footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,123 @@
+import { ReactNode } from 'react';
+import {
+  Box,
+  Container,
+  Stack,
+  SimpleGrid,
+  Text,
+  Link,
+  VisuallyHidden,
+  chakra,
+  useColorModeValue,
+} from '@chakra-ui/react';
+import { FaTwitter, FaYoutube, FaInstagram } from 'react-icons/fa';
+
+// import AppStoreBadge from '@/components/AppStoreBadge';
+// import PlayStoreBadge from '@/components/PlayStoreBadge';
+
+const ListHeader = ({ children }: { children: ReactNode }) => {
+  return (
+    <Text fontWeight={'500'} fontSize={'lg'} mb={2}>
+      {children}
+    </Text>
+  );
+};
+
+const SocialButton = ({
+  children,
+  label,
+  to,
+}: {
+  children: ReactNode;
+  label: string;
+  to: string;
+}) => {
+  return (
+    <chakra.button
+      bg={useColorModeValue('blackAlpha.100', 'whiteAlpha.100')}
+      rounded={'full'}
+      w={8}
+      h={8}
+      cursor={'pointer'}
+      as={'a'}
+      to={to}
+      display={'inline-flex'}
+      alignItems={'center'}
+      justifyContent={'center'}
+      transition={'background 0.3s ease'}
+      _hover={{
+        bg: useColorModeValue('blackAlpha.200', 'whiteAlpha.200'),
+      }}>
+      <VisuallyHidden>{label}</VisuallyHidden>
+      {children}
+    </chakra.button>
+  );
+};
+
+export default function Footer() {
+  return (
+    <Box
+      bg={useColorModeValue('gray.50', 'gray.900')}
+      color={useColorModeValue('gray.700', 'gray.200')}>
+      <Container as={Stack} maxW={'6xl'} py={10}>
+        <SimpleGrid columns={{ base: 1, sm: 2, md: 4 }} spacing={8}>
+          <Stack align={'flex-start'}>
+            <ListHeader>Company</ListHeader>
+            <Link to={'/non'}>About Us</Link>
+            <Link to={'/non'}>Blog</Link>
+            <Link to={'/non'}>Careers</Link>
+            <Link to={'/non'}>Contact Us</Link>
+          </Stack>
+
+          <Stack align={'flex-start'}>
+            <ListHeader>Support</ListHeader>
+            <Link to={'/non'}>Help Center</Link>
+            <Link to={'/non'}>Safety Center</Link>
+            <Link to={'/non'}>Community Guidelines</Link>
+          </Stack>
+
+          <Stack align={'flex-start'}>
+            <ListHeader>Legal</ListHeader>
+            <Link to={'/non'}>Cookies Policy</Link>
+            <Link to={'/non'}>Privacy Policy</Link>
+            <Link to={'/non'}>Terms of Service</Link>
+            <Link to={'/non'}>Law Enforcement</Link>
+          </Stack>
+
+          <Stack align={'flex-start'}>
+            <ListHeader>Install App</ListHeader>
+            {/* <AppStoreBadge />
+            <PlayStoreBadge /> */}
+          </Stack>
+        </SimpleGrid>
+      </Container>
+
+      <Box
+        borderTopWidth={1}
+        borderStyle={'solid'}
+        borderColor={useColorModeValue('gray.200', 'gray.700')}>
+        <Container
+          as={Stack}
+          maxW={'6xl'}
+          py={4}
+          direction={{ base: 'column', md: 'row' }}
+          spacing={4}
+          justify={{ md: 'space-between' }}
+          align={{ md: 'center' }}>
+          <Text>© 2022 Chakra Templates. All rights reserved</Text>
+          <Stack direction={'row'} spacing={6}>
+            <SocialButton label={'Twitter'} to={'/non'}>
+              <FaTwitter />
+            </SocialButton>
+            <SocialButton label={'YouTube'} to={'/non'}>
+              <FaYoutube />
+            </SocialButton>
+            <SocialButton label={'Instagram'} to={'/non'}>
+              <FaInstagram />
+            </SocialButton>
+          </Stack>
+        </Container>
+      </Box>
+    </Box>
+  );
+}
